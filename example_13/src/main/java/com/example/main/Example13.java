45941ceeaa71ca3d4cde1c3b9331d9859ade826f
Ashwin <136216570+ROMANASHWIN@users.noreply.github.com>
2025-02-19 08:38:41 +0530

    Add files via upload

---
+++ example_13/src/main/java/com/example/main/Example13.java
@@ -0,0 +1,25 @@
+package com.example.main;
+
+import com.example.beans.Person;
+import com.example.beans.Vehicle;
+import com.example.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Arrays;
+
+public class Example13 {
+
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        String[] persons = context.getBeanNamesForType(Person.class);
+        System.out.print(Arrays.toString(persons));
+
+        Person person = context.getBean(Person.class);
+        String[] names = context.getBeanNamesForType(Vehicle.class);
+        /*Vehicle vehicle = context.getBean(Vehicle.class);
+        vehicle.getVehicleServices().playMusic();
+        vehicle.getVehicleServices().moveVehicle();*/
+        person.getVehicle().getVehicleServices().playMusic();
+        person.getVehicle().getVehicleServices().moveVehicle();
+    }
+}
